364d6697b218d3f126be01eb06efd3f3a58beecb
Josh Taylor <jstaylor926@gmail.com>
2020-10-16 10:02:05 -0400

    searchbar-81

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
 
-import SeasonDisplay from './components/SeasonDisplay';
-import Spinner from './components/Spinner';
-
-
-
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
-
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
+const App = () => {
+  return (
+   <SearchBar />
+  );
 }
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+//  import Tester from './Tester';
+class SearchBar extends React.Component {
+    state = {
+        term: ''
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+               
+                    <form className="ui form">
+                        <div className="field">
+                            <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })} />
+                       
+                        </div>
+                    </form>
+               
+            </div>
+        )
+    }
+}
+export default SearchBar;

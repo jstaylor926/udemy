50603f0b4b410d1b232a4a1855269e423e98cde5
Josh Taylor <jstaylor926@gmail.com>
2020-10-19 09:54:51 -0400

    searchbar update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
 
-const App = () => {
-  return (
-   <SearchBar />
-  );
+class App extends React.Component {
+	onSearchSubmit(term) {
+		console.log(term);
+	}
+
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: '10px' }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
 }
 
 export default App;
